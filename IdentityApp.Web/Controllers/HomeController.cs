c70e20aeeb8deb42ff3c0fc9bd9a3fd6a2df4b05
Serkan IŞIK <dehnas79@gmail.com>
2023-09-19 12:03:36 +0300

    Add SignUpViewModel and SignUp.cshtml

--- IdentityApp.Web/Controllers/HomeController.cs
+++ IdentityApp.Web/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace IdentityApp.Web.Controllers
             return View();
         }
 
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
